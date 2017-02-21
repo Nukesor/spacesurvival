108af40ea9a31c02842301fdc692a151fb4f5b56
Arne Beer <arne@twobeer.de>
2017-02-21 20:50:14 +0100

    Source local env in watch sh

--- watch.sh
+++ watch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p static
+source ./local_env.sh
 elm-package install
 watchexec --exts rs,toml,sql --restart "./reset.sh && RUST_BACKTRACE=1 cargo run" &
 watchexec --exts elm --restart "elm-make client/Main.elm --output static/main.js" &
