2b8b7d53a3493efbaad66ccbf5514ca3cd44ecf0
Rafael Epplee <raffomania@gmail.com>
2017-02-21 01:50:07 +0100

    Add bg animation

--- client/View.elm
+++ client/View.elm
@@ -13,8 +13,11 @@ type Msg
 
 view : Model -> Html.Html msg
 view model =
-    div [ class "dialog-container" ]
-        [ Dialog.dialog
-            (Animation.render model.style)
-            [ h2 [] [ text "Welcome to SPACE" ], p [] [ text "Your journey is about to begin." ] ]
+    div []
+        [ div [ class "dialog-container" ]
+            [ Dialog.dialog
+                (Animation.render model.style)
+                [ h2 [] [ text "Welcome to SPACE" ], p [] [ text "Your journey is about to begin." ] ]
+            ]
+        , div [ class "bg-container" ] [ div [ class "bg" ] [] ]
         ]
