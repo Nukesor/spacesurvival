34bebb0f8ae97fe406eb28e96950726d839a33c5
Arne Beer <arne@twobeer.de>
2017-02-22 22:47:04 +0100

    Final code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-#[macro_use] extern crate lazy_static;
 extern crate uuid;
 extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
 extern crate serde_json;
-#[macro_use] extern crate serde_derive;
 extern crate validator;
-#[macro_use] extern crate validator_derive;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_codegen;
 extern crate jsonwebtoken;
 extern crate chrono;
 extern crate argon2rs;
 extern crate rustc_serialize;
 extern crate r2d2;
 extern crate r2d2_diesel;
+
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate validator_derive;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
 
 mod api;
 mod validation;
