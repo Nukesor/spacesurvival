87190b741d823765a8327824954dd3dc3be7f7c4
Arne Beer <arne@twobeer.de>
2017-03-11 14:10:18 +0100

    Better invalid json responses

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,17 +1,26 @@
-use rocket_contrib::{JSON};
-use validation::user::UserSerializer;
-use diesel::prelude::*;
+use std::error::Error;
+
 use diesel;
+use diesel::prelude::*;
+use rocket_contrib::{JSON, SerdeError};
 
 use models::user::{UserModel, NewUser};
 use schema::users;
-use helpers::db::DB;
 use schema::users::dsl::*;
-use responses::{APIResponse, ok, created, conflict, unauthorized};
+use validation::user::UserSerializer;
+
+use helpers::db::DB;
+use responses::{APIResponse, ok, created, conflict, unauthorized, bad_request};
 
 
 #[post("/login", data = "<user_in>", format = "application/json")]
-pub fn login(user_in: JSON<UserSerializer>, db: DB) -> APIResponse {
+pub fn login(user_in: Result<JSON<UserSerializer>, SerdeError>, db: DB) -> APIResponse {
+    match user_in {
+        Result::Err(err) => return bad_request().message(err.description()),
+        _ => (),
+    }
+    let user_in = user_in.unwrap();
+
     let results = users.filter(email.eq(user_in.email.clone()))
         .first::<UserModel>(&*db);
 
@@ -27,17 +36,24 @@ pub fn login(user_in: JSON<UserSerializer>, db: DB) -> APIResponse {
     ok().data(json!(user.generate_auth_token("loginsalt")))
 }
 
-#[post("/register", data = "<user>", format = "application/json")]
-pub fn register(user: JSON<UserSerializer>, db: DB) -> APIResponse {
-    let results = users.filter(email.eq(user.email.clone()))
+#[post("/register", data = "<user_data>", format = "application/json")]
+pub fn register(user_data: Result<JSON<UserSerializer>, SerdeError>, db: DB) -> APIResponse {
+
+    match user_data {
+        Result::Err(err) => return bad_request().message(err.description()),
+        _ => (),
+    }
+    let user_data = user_data.unwrap();
+
+    let results = users.filter(email.eq(user_data.email.clone()))
         .first::<UserModel>(&*db);
     if results.is_ok() {
         return conflict().message("User already exists.");
     }
 
-    let new_password_hash = UserModel::make_password_hash(user.password.as_str());
+    let new_password_hash = UserModel::make_password_hash(user_data.password.as_str());
     let new_user = NewUser {
-        email: user.email.clone(),
+        email: user_data.email.clone(),
         password_hash: new_password_hash,
     };
 
