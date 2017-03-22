b7ba2ce20019a78730340de9eb80f47438b0fe42
Arne Beer <arne@twobeer.de>
2017-03-22 22:01:18 +0100

    Allow auth with either nick or email

--- server/src/api/auth/auth.rs
+++ server/src/api/auth/auth.rs
@@ -3,14 +3,14 @@ use rocket_contrib::{JSON, SerdeError};
 
 use models::user::UserModel;
 use schema::users::dsl::*;
-use validation::user::UserSerializer;
+use validation::user::LoginSerializer;
 
 use helpers::db::DB;
 use responses::{APIResponse, ok, unauthorized, bad_request};
 
 
 #[post("/login", data = "<user_in>", format = "application/json")]
-pub fn login(user_in: Result<JSON<UserSerializer>, SerdeError>, db: DB) -> APIResponse {
+pub fn login(user_in: Result<JSON<LoginSerializer>, SerdeError>, db: DB) -> APIResponse {
 
     // Return specific error if invalid JSON has been sent.
     if user_in.is_err() {
@@ -19,14 +19,25 @@ pub fn login(user_in: Result<JSON<UserSerializer>, SerdeError>, db: DB) -> APIRe
 
     let user_in = user_in.unwrap();
 
-    let results = users.filter(email.eq(user_in.email.clone()))
-        .first::<UserModel>(&*db);
+    let result;
+    if user_in.nickname.is_some() {
+        result = users.filter(nickname.eq(user_in.nickname.as_ref().unwrap().clone()))
+            .first::<UserModel>(&*db);
+    }
+    else if user_in.nickname.is_some() {
+        result = users.filter(email.eq(user_in.email.as_ref().unwrap().clone()))
+            .first::<UserModel>(&*db);
+    }
+    else {
+        return bad_request()
+            .message("Either a nickname or an email needs to be specified");
+    }
 
-    if results.is_err() {
+    if result.is_err() {
         return unauthorized().message("Username or password incorrect.");
     }
 
-    let user = results.unwrap();
+    let user = result.unwrap();
     if !user.verify_password(user_in.password.as_str()) {
         return unauthorized().message("Username or password incorrect.");
     }
