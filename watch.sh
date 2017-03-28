66f732cfa70c350a03cbea6fe8bbfef10504511c
Rafael Epplee <raffomania@gmail.com>
2017-03-28 22:47:02 +0200

    Add npm install to watch.sh

--- watch.sh
+++ watch.sh
@@ -3,6 +3,7 @@
 mkdir -p static
 source ./local_env.sh
 elm-package install
+npm install
 watchexec --watch server --watch migrations --restart "./reset.sh && RUST_BACKTRACE=1 cargo run" &
 watchexec --watch client --restart "elm-make client/Main.elm --output static/main.js" &
 watchexec --watch assets "cp -r assets/* static/" &
