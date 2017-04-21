f310053d5e8ff78f202985fc274faa87140f0134
Arne Beer <arne@twobeer.de>
2017-04-21 17:39:27 +0200

    Macro for enum types creation with traits for from and to string

--- server/src/data/types.rs
+++ server/src/data/types.rs
@@ -1,20 +1,48 @@
-#[derive(Debug, Eq, Clone, Hash, PartialEq, Serialize, Deserialize)]
-pub enum ResearchTypes {
+use std::fmt;
+
+#[macro_export]
+macro_rules! enum_impl {
+    ($enumname: ident {
+        $($enumvals: ident,)*
+    }) => {
+        #[derive(Debug, Eq, Clone, Hash, PartialEq, Serialize, Deserialize)]
+        pub enum $enumname {
+            $($enumvals,)*
+        }
+
+        impl fmt::Display for $enumname {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                fmt::Debug::fmt(self, f)
+            }
+        }
+
+        impl $enumname {
+            #[allow(dead_code)]
+            pub fn from_str(name: &str) -> Result<$enumname, ()> {
+                match name {
+                    $(
+                        stringify!($enumvals) => Ok($enumname::$enumvals),
+                    )*
+                    _ => Err(()),
+                }
+            }
+
+        }
+    }
+}
+
+enum_impl!{ResearchTypes {
     PlasmaGenerator,
     EnergyWeapons,
     MiningEfficiency,
-}
+}}
 
-
-#[derive(Debug, Eq, Clone, Hash, PartialEq, Serialize, Deserialize)]
-pub enum ResourceTypes {
+enum_impl!{ResourceTypes {
     Iron,
     Water,
-}
+}}
 
-
-#[derive(Debug, Eq, Clone, Hash, PartialEq, Serialize, Deserialize)]
-pub enum ModuleTypes {
+enum_impl!{ModuleTypes {
     Turret,
     Generator,
-}
+}}
