0a83c2f1fdcafcc9c00cf29e64bc3bc855959cff
Rafael Epplee <raffomania@gmail.com>
2017-09-02 22:00:07 +0200

    Add mineral resource image

--- client/View/Resources.elm
+++ client/View/Resources.elm
@@ -5,6 +5,8 @@ import Css.Namespace exposing (namespace)
 import Html exposing (..)
 import Html.CssHelpers
 import Model.Resources exposing (Resource)
+import Svg exposing (image, svg)
+import Svg.Attributes
 
 
 view model =
@@ -13,7 +15,24 @@ view model =
 
 item : Resource -> Html msg
 item resource =
-    li [] [ Html.text ((toString resource.amount) ++ "/" ++ (toString resource.maxAmount) ++ " " ++ resource.name) ]
+    li []
+        [ resourceImage resource
+        , Html.text ((toString resource.amount) ++ "/" ++ (toString resource.maxAmount) ++ " " ++ resource.name)
+        ]
+
+
+resourceImage resource =
+    svg
+        [ Svg.Attributes.width "40px"
+        , Svg.Attributes.height "40px"
+        ]
+        [ image
+            [ Svg.Attributes.xlinkHref "/static/img/minerals.svg"
+            , Svg.Attributes.width "100%"
+            , Svg.Attributes.height "100%"
+            ]
+            []
+        ]
 
 
 type Classes
