540c9fa1e5def39ea08dc051a7c4411bc76ab417
Arne Beer <arne@twobeer.de>
2017-11-22 23:04:11 +0100

    Uwsgi tick

--- server/tick.py
+++ server/tick.py
@@ -1,8 +1,11 @@
 """Server entry point."""
+from uwsgidecorators import timer
 from server.app import app # noqa
-from server.worker.queue import QueueUpdater
+from server.worker.queue import finished_entries
 
-if __name__ == '__main__':
+
+@timer(1)
+def update_queues(arg):
+    """Update queue related stuff."""
     with app.app_context():
-        updater = QueueUpdater()
-        updater.run()
+        finished_entries()
--- server/worker/queue.py
+++ server/worker/queue.py
@@ -5,29 +5,22 @@ from server.extensions import db
 from server.models import QueueEntry
 
 
-class QueueUpdater():
-    """Class for updating queue."""
+def finished_entries():
+    """Process finished entries."""
+    queue_entries = db.session.query(QueueEntry) \
+        .filter(QueueEntry.finishes_at <= datetime.now()) \
+        .all()
 
-    def run(self):
-        """Tick."""
-        self.finished_entries()
+    for entry in queue_entries:
+        if entry.module:
+            entry.module.level = entry.level
+            db.session.add(entry.module)
+            entry.module.pod.update_resources()
 
-    def finished_entries(self):
-        """Process finished entries."""
-        queue_entries = db.session.query(QueueEntry) \
-            .filter(QueueEntry.finishes_at <= datetime.now()) \
-            .all()
+        elif entry.research:
+            entry.research.level += 1
+            db.session.add(entry.research)
 
-        for entry in queue_entries:
-            if entry.module:
-                entry.module.level = entry.level
-                db.session.add(entry.module)
-                entry.module.pod.update_resources()
-
-            elif entry.research:
-                entry.research.level += 1
-                db.session.add(entry.research)
-
-            entry.queue.next_entry()
-            db.session.delete(entry)
-        db.session.commit()
+        entry.queue.next_entry()
+        db.session.delete(entry)
+    db.session.commit()
